ef925c5661372ee045c565834f90f3eca5b417ce
Chirag Gupta <chirag@rcvtechnologies.com>
2024-12-13 15:13:33 +0530

    Code update

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -13,6 +13,7 @@ import Link from "next/link";
 import { sidebarLinks } from "@/constants";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import Footer from "./Footer";
 
 const MobileNav = ({ user }: MobileNavProps) => {
   const pathname = usePathname();
@@ -82,7 +83,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                 USER
               </nav>
             </SheetClose>
-            Footer
+            <Footer user={user} type="mobile"/>
           </div>
         </SheetContent>
       </Sheet>
